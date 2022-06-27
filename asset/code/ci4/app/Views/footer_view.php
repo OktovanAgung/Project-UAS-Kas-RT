32c29dc03fa096863fa6b260d71117fb5e49f3a6
OktovanAgung <oktovanagung@mhs.pelitabangsa.ac.id>
2022-06-27 14:03:56 +0700

    feat: create footer_view.php, header_view.php and warga_view.php

---
+++ asset/code/ci4/app/Views/footer_view.php
@@ -0,0 +1,6 @@
+            <footer>
+                <p>&copy; 2022 - Universitas Pelita Bangsa | Oktovan Agung Shailendra - 312010131 -  TI.20.A.1</p>
+            </footer>
+        </div>
+    </body>
+</html>
