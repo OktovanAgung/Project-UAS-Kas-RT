ecf2e0e28fff0b9a78b632cbe291e8de72265c8c
OktovanAgung <oktovanagung@mhs.pelitabangsa.ac.id>
2022-06-27 17:28:48 +0700

    update header_view.php and style.css

--- asset/code/ci4/app/Views/header_view.php
+++ asset/code/ci4/app/Views/header_view.php
@@ -2,15 +2,13 @@
 <html lang="en">
   <head>
     <title><?= $title;?></title>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-info">
+    <nav class="navbar navbar-expand-sm navbar-dark">
         <div class="container">
             <a class="navbar-brand" href="<?= base_url();?>">Data Warga RT 01 RW 03</a>
             <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
@@ -31,4 +29,3 @@
 			</div>
         </div>
     </nav>
-    
