be88026aee37e2b7d6097dd3a57a027d8f7e756c
OktovanAgung <oktovanagung@mhs.pelitabangsa.ac.id>
2022-07-08 21:14:28 +0700

    update README.md and kasrt.sql

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 |Kelas  | TI.20.A.1               |
 
 # UAS Pemrograman Web Semester 4 <br> Dosen Pembimbing : Agung Nugroho,S.Kom.,M.Kom., <br> Membuat Aplikasi Pengelolaan Iuran Kas RT
+
+## Link
+- [Video Dokumentasi](https://youtu.be/PrW8x6spSm8)
+
--- kasrt.sql
+++ kasrt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2022 at 08:38 AM
+-- Generation Time: Jul 08, 2022 at 05:34 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -31,10 +31,20 @@ CREATE TABLE `iuran` (
   `id` int(20) NOT NULL,
   `keterangan` tinytext NOT NULL,
   `tanggal` date NOT NULL,
+  `bulan` int(2) NOT NULL,
   `tahun` year(4) NOT NULL,
   `jumlah` decimal(10,2) NOT NULL,
   `warga_nik` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `iuran`
+--
+
+INSERT INTO `iuran` (`id`, `keterangan`, `tanggal`, `bulan`, `tahun`, `jumlah`, `warga_nik`) VALUES
+(1, 'Iuran Kas', '2022-07-07', 0, 2022, '50000000.00', '312010158'),
+(2, 'Tunggakan', '2022-07-07', 6, 2022, '5000.00', '312010100'),
+(3, 'Tunggakan', '2022-07-07', 6, 2022, '10000.00', '312010199');
 
 -- --------------------------------------------------------
 
@@ -45,12 +55,23 @@ CREATE TABLE `iuran` (
 CREATE TABLE `warga` (
   `id` int(20) NOT NULL,
   `nik` varchar(50) NOT NULL,
-  `nama` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
   `kelamin` enum('L','p') NOT NULL,
   `alamat` tinytext NOT NULL,
   `no_rumah` varchar(10) NOT NULL,
   `status` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `warga`
+--
+
+INSERT INTO `warga` (`id`, `nik`, `nama`, `kelamin`, `alamat`, `no_rumah`, `status`) VALUES
+(1, '3120010131', 'Oktovan Agung Shailendra', 'L', 'Cikarang', '22', '1'),
+(2, '312010158', 'Tatang', 'L', 'Sneznaya', '99', '1'),
+(3, '312010182', 'HuTao', 'p', 'Liyue', '77', '1'),
+(4, '312010100', 'Mona', 'p', 'Mondstadt', '01', '1'),
+(5, '312010199', 'Zhongli', 'L', 'Liyue', '06', '1');
 
 --
 -- Indexes for dumped tables
@@ -76,13 +97,13 @@ ALTER TABLE `warga`
 -- AUTO_INCREMENT for table `iuran`
 --
 ALTER TABLE `iuran`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `warga`
 --
 ALTER TABLE `warga`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
